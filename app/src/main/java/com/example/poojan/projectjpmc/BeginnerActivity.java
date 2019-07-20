3527ca8ee315e249469c603ff5be073e9b91342e
Poojan <turakhiapoojan@gmail.com>
2019-07-21 01:14:35 +0530

    commit for dishank

--- app/src/main/java/com/example/poojan/projectjpmc/BeginnerActivity.java
+++ app/src/main/java/com/example/poojan/projectjpmc/BeginnerActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
 
 public class BeginnerActivity extends AppCompatActivity {
 
     private CardView attendance, savings, stars, activity, viewDatabase, downloadRecords;
+    Button attedit, attview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +23,23 @@ public class BeginnerActivity extends AppCompatActivity {
         activity = findViewById(R.id.activity);
         viewDatabase = findViewById(R.id.viewDatabase);
         downloadRecords = findViewById(R.id.downloadRecords);
+        attedit = findViewById(R.id.att_edit);
+        attview = findViewById(R.id.att_view);
 
-        attendance.setOnClickListener(new View.OnClickListener() {
+        attedit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(BeginnerActivity.this, AttendanceActivity.class);
                 startActivity(i);
             }
         });
-
+        attview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(BeginnerActivity.this, ViewAttendanceActivity.class);
+                startActivity(i);
+            }
+        });
         viewDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
