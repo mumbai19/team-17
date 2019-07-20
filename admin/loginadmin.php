48d0cb3227bd76f62569a6272ea015919b3af5b8
kchawla98 <2016.kajol.chawla@ves.ac.in>
2019-07-20 20:43:01 +0530

    dashboard

--- admin/loginadmin.php
+++ admin/loginadmin.php
@@ -3,3 +3,12 @@
 <html>
 <title>Admin Dashboard</title>
 <body>
+<h1 align="center">ADMIN DASHBOARD</h1>
+<a href="viewReports.php" class="btn btn-info" role="button">Reports</a>
+<br>
+<br>
+<a href="addMentor.php" class="btn btn-info"
+   role="button">Allocate Mentors</a>
+    </body>
+</html>
+    
